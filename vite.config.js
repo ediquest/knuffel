fc80aafa101d3397670efaaf7bfabed519a03bff
ediquest <a.sarczynski@gmail.com>
2025-08-25 20:28:43 +0200

    vite cfg

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // USTAW base na '/NAZWA-REPO/' dla GitHub Pages project site
 export default defineConfig({
   plugins: [react()],
-  base: '/NAZWA-REPO/',
+  base: '/knuffel/',
 })
